614ea83b27161c024485186fc18a18b3299d3611
madvin <madvin@abv.bg>
2025-01-18 10:59:26 +0200

    initial first set

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('velikan')
+});
+
+app.listen(5000, () => console.log('Server is working (daaa) on http://localhost:5000...'));
+
