ea36aa0a6e7c9f7d904163b41550cb045a3d1fd6
madvin <madvin@abv.bg>
2025-01-21 19:51:46 +0200

    add routes module

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
-import homeController from './controllers/homeController.js';
+import routes from './routes.js';
 
 const app = express();
 
@@ -12,27 +12,7 @@ app.set('views', './src/views');
 
 app.use('/static', express.static('src/public'));
 
-app.use(homeController);
-
-app.get('/', (req, res) => {
-    res.render('home');
-});
-
-app.get('/about', (req, res) => {
-    res.render('about');
-});
-
-app.get('/create', (req, res) => {
-    res.render('create');
-});
-
-app.get('/search', (req, res) => {
-    res.render('search');
-});
-
-app.get('*', (req, res) => {
-    res.render('404');
-});
+app.use(routes);
 
 app.listen(5000, () => console.log('Server is working on http://localhost:5000...'));
 
