1274b6e5e495bc1219d8f8ff73de5175ea0041ff
madvin <madvin@abv.bg>
2025-01-25 15:46:28 +0200

    add movie controllers

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -10,4 +10,23 @@ movieController.get('/search', async (req, res) => {
     res.render('search', { movies, filter });
 });
 
+movieController.get('/create', (req, res) => {
+    res.render('create');
+});
+
+movieController.post('/create', async (req, res) => {
+    const newMovie = req.body;
+
+    await movieService.create(newMovie);
+
+    res.redirect('/');
+});
+
+movieController.get('/:movieId/details', async (req, res) => {
+    const movieId = req.params.movieId;
+    const movie = await movieService.getOneWithCasts(movieId);
+
+    res.render('movie/details', { movie });
+});
+
 export default movieController;
