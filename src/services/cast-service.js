4ff3237d8128dbe855d19b8568817640a836b79d
madvin <madvin@abv.bg>
2025-01-25 17:36:04 +0200

    fixed cast service

---
+++ src/services/cast-service.js
@@ -0,0 +1,15 @@
+import Cast from '../models/Cast.js';
+
+export default {
+    getAll(filter = {}) {
+        let query = Cast.find({});
+
+        if (filter.exclude) {
+            query = query.nin('_id', filter.exclude);
+        }
+        return query;
+    },
+    create(castData) {
+        return Cast.create(castData);
+    },
+}
