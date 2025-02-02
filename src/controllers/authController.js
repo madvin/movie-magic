5f33008f1ce27159ab1348cfc89dceec9948137a
madvin <madvin@abv.bg>
2025-02-02 17:33:03 +0200

    some updates

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -18,5 +18,17 @@ authController.get('/login', (req, res) => {
     res.render('auth/login');
 });
 
+authController.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    try{
+        await authService.login(email, password);
+    } catch (err) {
+        console.log(err.message);
+        return res.redirect('/404');
+    }
+
+    res.redirect('/');
+});
 
 export default authController;
--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -1,7 +1,35 @@
 import User from '../models/User.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const SECRET = 'NeshhtOToSuperS3cre7noosDSffasiTetakakte0vc4';
 
 export default {
     register(userData) {
         return User.create(userData);
+    },
+
+    async login(email, password) {
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            throw new Error('Username or password is invalid')
+        }
+
+        const isValid = await bcrypt.compare(password, user.password);
+
+        if (!isValid) {
+            throw new Error('Username or password is invalid');
+        }
+
+        const payload = {
+            id: user.id,
+            email: user.email
+        };
+
+        const token = jwt.sign(payload, SECRET, { expiresIn: '2h' })
+
+        return;
     }
 };
