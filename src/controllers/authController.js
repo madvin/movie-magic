0234688507921e83fa0892af81f0698dff14a8f9
madvin <madvin@abv.bg>
2025-02-02 17:36:08 +0200

    add jwt token to login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -22,7 +22,7 @@ authController.post('/login', async (req, res) => {
     const { email, password } = req.body;
 
     try{
-        await authService.login(email, password);
+        const token = await authService.login(email, password);
     } catch (err) {
         console.log(err.message);
         return res.redirect('/404');
